3f6c656c2cb18ea8a07f158af98171f8ab0c85c5
Mathaeus Sander <metti@kermit>
2025-04-17 22:26:10 +0200

    more advanced project config

--- src/app.cljs
+++ src/app.cljs
@@ -2,11 +2,16 @@
   (:require
    ["xstream$default" :as xs]
    ["@cycle/run" :refer [run]]
-   ["@cycle/dom" :refer [makeDOMDriver h1]]))
+   ["@cycle/dom" :refer [makeDOMDriver div input p]]))
 
-(defn main []
-  {:DOM (-> xs
-            (.periodic 1000)
-            (.map #(h1 (str % " bla seconds elapsed"))))})
+(defn main [sources]
+  {:DOM (-> sources.DOM
+            (.select "input")
+            (.events "change")
+            (.map #(-> % .-target .-checked))
+            (.startWith false)
+            (.map #(div [(input {:attrs {:type "checkbox"}})
+                         "Toggle Me"
+                         (p (if % "on" "off"))])))})
 
 (run main {:DOM (makeDOMDriver "#mount")})
